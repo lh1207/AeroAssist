2e0e3f5b3a1202e7eacc96b5fc84e0caa39eef76
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-02-07 15:35:02 -0500

    setup _Layout.cshtml via MS Doc https://learn.microsoft.com/en-us/aspnet/core/mvc/views/layout?view=aspnetcore-8.0

---
+++ Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AeroAssist.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
