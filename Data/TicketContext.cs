4b89506c50617b30fff1de9d8082a0b2c30a5053
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-02-06 19:32:34 -0500

    create controller, dbcontext, and view for 'Ticket'

---
+++ Data/TicketContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AeroAssist;
+
+namespace AeroAssist.Data
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext (DbContextOptions<TicketContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AeroAssist.Ticket> Ticket { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AeroAssist.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TicketContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TicketContext") ?? throw new InvalidOperationException("Connection string 'TicketContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
