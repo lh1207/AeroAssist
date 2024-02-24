2e04cfc9544596a3ba4d7636f656d4748c03e1f6
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-02-24 16:52:54 -0500

    Initialize redirect in CreateTicket via PageModel; easier implementation than using JS

--- Pages/CreateTicket.cshtml.cs
+++ Pages/CreateTicket.cshtml.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AeroAssist.Pages;
 
 public class CreateTicket : PageModel
 {
-    public void OnGet()
+    public class CreateTicketModel(HttpClient client, Ticket ticket) : PageModel
     {
-        
+        [BindProperty]
+        public Ticket Ticket { get; set; } = ticket;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var response = await client.PostAsJsonAsync("https://localhost:7223/api/Ticket/Ticket", Ticket);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("/Success");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "An error occurred while creating the ticket.");
+                return Page();
+            }
+        }
     }
 }
