99f61714aa80f9b485130f3907a818b11bff4e17
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-03-12 22:48:32 -0400

    add CRUD to model; add success page

--- Models/TicketModel.cs
+++ Models/TicketModel.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -17,11 +18,12 @@ namespace AeroAssist.Models
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            var response = await _client.GetAsync($"https://localhost:7223/swagger/api/Ticket/{id}");
+            var response = await _client.GetAsync($"https://localhost:7223/api/Ticket/{id}");
             if (response.IsSuccessStatusCode)
             {
                 var json = await response.Content.ReadAsStringAsync();
                 Tickets = JsonConvert.DeserializeObject<IEnumerable<Ticket>>(json);
+                return RedirectToPage("Success");
             }
             else
             {
@@ -29,6 +31,49 @@ namespace AeroAssist.Models
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Ticket ticket)
+        {
+            var json = JsonConvert.SerializeObject(ticket);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("https://localhost:7223/api/Ticket", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("Index");
+            }
+            // else
+            {
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPutAsync(Ticket ticket)
+        {
+            var json = JsonConvert.SerializeObject(ticket);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync("https://localhost:7223/api/Ticket", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("Index");
+            }
+            // else
+            {
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnDeleteAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"https://localhost:7223/api/Ticket/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("Index");
+            }
+            // else
+            {
+                return Page();
+            }
         }
     }
 }
