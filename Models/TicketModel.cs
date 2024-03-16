26e2996f65b8f23f5370d62c832ef880a8d6bbbe
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-03-16 02:15:01 -0400

    clarify OnPost prerequisite

--- Models/TicketModel.cs
+++ Models/TicketModel.cs
@@ -23,7 +23,6 @@ namespace AeroAssist.Models
         }
         // needed for OnGet
         public List<Ticket>? Tickets { get; set; }
-        public Ticket Ticket { get; set; }
 
         public async Task OnGet()
         {
@@ -45,6 +44,9 @@ namespace AeroAssist.Models
                 Tickets = new List<Ticket>(); // Initialize Tickets as an empty list
             }
         }
+
+        // needed for OnPost
+        public Ticket Ticket { get; set; }
 
         // Post to create a new ticket via API endpoint then redirect to success page if success. If not, redirect
         // to error page and show error.
