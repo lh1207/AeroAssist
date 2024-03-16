3c13f41868e59b6dc2b921d8076a20e8caf5761c
lh1207 <100445409+lh1207@users.noreply.github.com>
2024-03-16 00:20:55 -0400

    reach OnPostAsync method breakpoint successfully. initialize error handling and user prompting

---
+++ Models/ErrorModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AeroAssist.Models;
+
+public class ErrorModel : PageModel
+{
+    public string ErrorCode { get; set; }
+
+    public void OnGet(string errorCode)
+    {
+        ErrorCode = errorCode;
+    }
+}
